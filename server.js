104273d08951ab17c10040c6fc4f17d84f5b2a23
AlisaEx <eatliightning@gmail.com>
2013-08-12 15:28:06 -0400

    added index.js for modularity purposes

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var http = require("http");
-
-function respond(request, response){
-	console.log("Request recieved.")
-	response.writeHead(200, {"content-Type": "text/plain"});
-	response.write("Hello World!");
-	response.end();
+function start(){
+	function respond(request, response){
+		console.log("Request recieved.")
+		response.writeHead(200, {"content-Type": "text/plain"});
+		response.write("Hello World!");
+		response.end();
+	}
+	http.createServer(respond). listen(8888);
+	console.log("The server has started.")
 }
 
-http.createServer(respond). listen(8888);
-console.log("The server has started.")
+exports.start = start;
