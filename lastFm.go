ecd8019f17e5cd3e0c89248ce258a0f1956fefcd
AlisaEx <eatliightning@gmail.com>
2013-08-12 14:30:16 -0400

    node hello world added

--- lastFm.go
+++ lastFm.go
@@ -1,33 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
-	"os"
-	"log"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
-const api_key = "8148fb40ef9511752203d2c4591e63d0"
-const user = "alisatrocity"
-
-func check( err error ){
-	if err != nil{
+func check(err error) {
+	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
 }
-type TopArtists struct{
-	top string `json: "topartists:"`
-	artist []Artists `json: "topartists>artist:"`
+
+type TopArtists struct {
+	Artist []Artists `json: "topartists>artist"`
 }
 
-type Artists struct{
-	Name string `json: "name:"`
-	Playcount string `json: "playcount:"`
+type Artists struct {
+	Name      string `json: "name"`
+	Playcount string `json: "playcount"`
 }
-func DownloadUrl(url string) []bytes){
+
+func DownloadUrl(url string) {
 	resp, err := http.Get(url)
 	check(err)
 	defer resp.Body.Close()
@@ -36,14 +34,12 @@ func DownloadUrl(url string) []bytes){
 	return data
 }
 
-
-func main(){
-	data := DownloadUrl("ws.audioscrobbler.com/2.0/?format=json&api_key="+api_key+"&method=user.getTopArtists&user="+user)
+func main() {
+	data := DownloadUrl("http://ws.audioscrobbler.com/2.0/?format=json&api_key=8148fb40ef9511752203d2c4591e63d0&method=user.getTopArtists&user=alisatrocity")
 	a := new(TopArtists)
 	err := json.Unmarshal(data, a)
 	check(err)
-	fmt.Println(a.top)
-	for _, k := range a.artist {
-		fmt.Printf("Artist: %s-%s\n", k.Name, k.Playcount)
+	for _, k := range a.Artist {
+		fmt.Printf("Artist: %s %s\n", k.Name, k.Playcount)
 	}
 }
