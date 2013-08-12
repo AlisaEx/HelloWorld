379ef8ecd0fdb5da3bea06094b50b6574fd655d9
AlisaEx <eatliightning@gmail.com>
2013-08-12 17:50:39 -0400

    added exec

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,12 @@
+var exec = require("child_process").exec;
+
 function start(){
 	console.log("Request handler 'start' was called.");
-	function sleep(milliseconds){
-		var startTime = new Date().getTime();
-		while(new Date().getTime < startTime+milliseconds);
-	}
-	sleep(1000);
-	return "Hello Start";
+	var content = "empty";
+	exec("find", function(error, stdout, stderr){
+		content = stdout;
+	})
+	return content;
 }
 function upload(){
 	console.log("Request handler 'upload' was called.");
