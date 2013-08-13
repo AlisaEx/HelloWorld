1df40e9cc5bd5e1f8f56e4350123f830d3d0592b
AlisaEx <eatliightning@gmail.com>
2013-08-13 00:23:39 -0400

    passing response around

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var http = require("http");
 var url = require("url");
 function start(route, handle){
-	function respond(request, response){
+	function onRequest(request, response){
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " recieved.")
-		response.writeHead(200, {"content-Type": "text/plain"});
-		var content = route(handle, pathname);
-		response.write(content);
-		response.end();
+		route(handle, pathname, response);
 	}
-	http.createServer(respond). listen(8888);
+	http.createServer(onRequest). listen(8888);
 	console.log("The server has started.")
 }
 
